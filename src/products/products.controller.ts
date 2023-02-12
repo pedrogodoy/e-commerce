9e4d665eadf8f3a7a7737301ddca8a8f88a077e1
Pedro <pedrogh.godoy12@gmail.com>
2023-02-12 00:27:28 -0300

    feat: update product

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,19 +1,25 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { ProductsService } from './products.service';
 import { Product } from './interfaces/product.interface';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  async create(@Body() createCatDto: CreateProductDto) {
-    return this.productsService.create(createCatDto);
+  async create(@Body() createProductDto: CreateProductDto) {
+    return this.productsService.create(createProductDto);
   }
 
   @Get()
   async findAll(): Promise<Product[]> {
     return this.productsService.findAll();
+  }
+
+  @Put('/:id')
+  async update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+    return this.productsService.update(id, updateProductDto);
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './interfaces/product.interface';
 
 @Injectable()
@@ -14,5 +15,19 @@ export class ProductsService {
 
   async findAll(): Promise<Product[]> {
     return this.productModel.find().exec();
+  }
+
+  async update(id: string, updateProductDto: UpdateProductDto): Promise<Product> {
+    if (id) {
+      const filter = { product_id: id };
+      const update = { ...updateProductDto };
+     
+      const updatedProduct = await this.productModel.findOneAndUpdate(filter, update, {
+        new: true
+      });
+      return updatedProduct;
+    }
+
+    throw Error('ID necessário');
   }
 }
